40e61e0840dcde979fad02d49aa76387a49347b0
Afanas2707 <avg57327@gmail.com>
2023-11-06 20:47:31 +0300

    first

--- homework/main.py
+++ homework/main.py
@@ -1,20 +1,4 @@
 import re
-import requests
-from requests_html import HTMLSession
-
-# Получаем HTML-код страницы
-#session = HTMLSession()
-#url = 'https://www.randomlists.com/email-addresses'
-#EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
-#r = session.get(url)
-#r.html.render()
-#for re_match in re.finditer(EMAIL_REGEX, r.html.raw_html.decode()):
-#    print(re_match.group())
-
-
-
-#response = requests.get(url)
-#html = response.text
 
 with open("Random Email Addresses.html", "r", encoding="utf-8") as file:
     html = file.read()
